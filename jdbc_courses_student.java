91422211cf93086081ca606c8eb14cf4eb6ba40f
scottj98 <44309841+scottj98@users.noreply.github.com>
2020-04-20 15:51:51 -0500

    Update jdbc_courses_student.java

--- jdbc_courses_student.java
+++ jdbc_courses_student.java
@@ -13,12 +13,12 @@ public class jdbc_courses_student {
 		String query1 = "SELECT * FROM COURSE;";
 		builder.append("<br> Lists of COURSES: " + myDB.query(query1) + "<br>");
 		
-		String studentID = "";
+		String studentID = "000";
 		studentID = args[0];
 		
-		String query2 = "SELECT * FROM COURSE WHERE STUDENTID = " + studentID + ";";
+		String query2 = "SELECT * FROM ENROLLMENT WHERE STUDENTID = " + studentID + ";";
 		
-		builder.append("<br><br><br> Courses for STUDENTID: " + studentID + myDB.query(query2));
+		builder.append("<br><br><br> Courses for STUDENT ID (" + studentID + "):" + myDB.query(query2) + "<br>");
 		System.out.println(builder.toString());
 		
 		myDB.disConnect();
